2e225cf80d2f930edd9fd9b155d784f9e7780fc2
BuildTools <unconfigured@null.spigotmc.org>
2024-03-28 18:55:03 -0500

    refactored for use with mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,12 @@
-import { Client, GatewayIntentBits, GuildMember } from "discord.js";
+import { Client, Events, GatewayIntentBits, GuildMember } from "discord.js";
 import { env } from "process";
 import "./setup.js";
 import { connect } from "mongoose";
+import GuildModel from "./model/guild.js";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildModeration] });
 
 await connect(process.env['MONGO_URL'] + process.env['DB_NAME'])
-
-type GuildMemberMap = {
-    [guildID: string]: { id: string, barkcount: number }[]
-}
-
-const maxBarks = 3
-
-const badDogList: GuildMemberMap = {}
 
 const barkList = [
     "Woof!",
@@ -28,28 +21,28 @@ client.once('ready', () => {
     console.log(`logged in as:\t${client.user.tag}`);
 })
 
-client.on('messageCreate', async message => {    
-
-    if (!badDogList[message.guild.id]) badDogList[message.guild.id] = []
+client.on(Events.MessageCreate, async message => {   
+    
+    const guildModel = await GuildModel.findOne({guildID: message.guildId})
+    const badDog = guildModel.badDogs.find(user => user.userID === message.author.id)
 
     if (message.mentions.users.has(client.user.id) || message.mentions.repliedUser === client.user) {
-        if (badDogList[message.guild.id].filter(user => user.id === message.author.id).length < 1) {
-            badDogList[message.guild.id].push({ id: message.author.id, barkcount: 0 })
+        if (!badDog) {
+            guildModel.updateOne({'$push': { badDogs: { userID: message.author.id, barkCount: 0 }}})
         }
     }
 
-    if (badDogList[message.guild.id].filter(user => message.author.id === user.id).length === 1) {
-        const badDogIndex = badDogList[message.guild.id].map(user => user.id).indexOf(message.author.id)
-        badDogList[message.guild.id][badDogIndex].barkcount++
-        const barkcount = badDogList[message.guild.id][badDogIndex].barkcount
-        console.log(`${message.author.id} is a bad dog.\tUserID:${badDogList[message.guild.id][badDogIndex].id}\tBarkCount:${badDogList[message.guild.id][badDogIndex].barkcount}`);
+    if (badDog) {
+        const barkCount = badDog.barkCount++
+        GuildModel.updateOne({ _id: guildModel._id, 'badDogs.userID': badDog.userID }, { '$set': { 'badDogs.$.barkCount': barkCount} })
+        console.log(`${message.author.id} is a bad dog.\tUserID:${badDog.userID}\tBarkCount:${badDog.barkCount}`);
 
 
         const replyText = `# **${barkList[Math.floor(Math.random() * barkList.length)]}**\n\n` +
-        `This is your ${barkcount}/${maxBarks * (Math.floor((barkcount - 1) / maxBarks) + 1)} strike. ${barkcount % maxBarks === 0 ? 'Goodbye for now!' : ''}`
+        `This is your ${barkCount % guildModel.maxBarks}/${guildModel.maxBarks} strike. ${barkCount % guildModel.maxBarks === 0 ? 'Goodbye for now!' : ''}`
         message.reply(replyText)
 
-        if (barkcount % maxBarks === 0) {
+        if (barkCount % guildModel.maxBarks === 0) {
             try {
                 await message.member.timeout(60_000)
             } catch (e) {
@@ -60,22 +53,23 @@ client.on('messageCreate', async message => {
     }
 })
 
-client.on('interactionCreate', async interaction => {
+client.on(Events.InteractionCreate, async interaction => {
     console.log("\n\n\n---------interaction received-------\n");
-
-    if (!badDogList[interaction.guild.id]) badDogList[interaction.guild.id] = []
 
     if (!interaction.isChatInputCommand()) return
 
+    const guildModel = await GuildModel.findOne({guildID: interaction.guildId})
+
     let pup = interaction.options.getUser('bad_dog') || interaction.options.getUser('good_pupper')
+    const badDog = guildModel.badDogs.find(user => user.userID === pup.id)
 
     switch (interaction.commandName) {
         case `sic`:
             if (pup.id !== client.user.id) {
                 console.log(`the Bad Dog ${pup.displayName}:${pup.id} is being sicced`);
 
-                if (badDogList[interaction.guild.id].filter(user => user.id === pup.id).length < 1) {
-                    badDogList[interaction.guild.id].push({ id: pup.id, barkcount: 0 })
+                if (!badDog) {
+                    guildModel.updateOne({'$push': { badDogs: { userID: pup.id, barkCount: 0 }}})
                 }
 
                 interaction.reply({ content: `User ${pup} is being sicced now!`, ephemeral: true })
@@ -86,10 +80,10 @@ client.on('interactionCreate', async interaction => {
             }
             break
         case `unsic`:
-            if (badDogList[interaction.guild.id].filter(user => user.id === pup.id).length === 1) {
+            if (badDog) {
                 console.log(`the Good Dog ${pup.displayName}:${pup.id} is being unsicced`);
 
-                badDogList[interaction.guild.id].splice(badDogList[interaction.guild.id].map(user => user.id).indexOf(pup.id), 1)
+                guildModel.updateOne({'$pull': { badDogs: { userID: pup.id }}})
 
                 interaction.reply({ content: `The improved pupper ${pup}, has been given a break`, ephemeral: true })
             } else {
@@ -101,7 +95,7 @@ client.on('interactionCreate', async interaction => {
         case `settle`:
             console.log(`Settling down.`);
 
-            badDogList[interaction.guild.id].splice(0, badDogList[interaction.guild.id].length)
+            guildModel.updateOne({'$set': { 'badDogs': [] }})
 
             interaction.reply({ content: `${client.user} has settled down.`, ephemeral: true })
 
@@ -109,39 +103,53 @@ client.on('interactionCreate', async interaction => {
         case 'pet':
             console.log(`Appreciating the pet.`);
 
-            if (badDogList[interaction.guild.id].filter(user => user.id === interaction.user.id).length === 1) {
+            if (badDog) {
                 console.log(`Deciding ${interaction.user.displayName}'s fate`)
 
-                Math.floor(Math.random() * 100) > 72 ? (() => {
-                    interaction.reply('# Wags Tail \n This shall do.')
-                    badDogList[interaction.guild.id].splice(badDogList[interaction.guild.id].map(user => user.id).indexOf(interaction.user.id), 1)
+                if (Math.floor(Math.random() * 100) > 72) {
+                    guildModel.updateOne({'$pull': { badDogs: { userID: pup.id }}})
                     console.log(`${interaction.user.displayName} was let off the hook`)
-                })() : (async () => {
-                    const badDogIndex = badDogList[interaction.guild.id].map(user => user.id).indexOf(interaction.user.id)
-                    badDogList[interaction.guild.id][badDogIndex].barkcount++
-                    const barkcount = badDogList[interaction.guild.id][badDogIndex].barkcount
-                    console.log(`${interaction.user.id} is a bad dog.\tUserID:${badDogList[interaction.guild.id][badDogIndex].id}\tBarkCount:${badDogList[interaction.guild.id][badDogIndex].barkcount}`);
-
+                    interaction.reply('# Wags Tail \n This shall do.')
+                } else {
+                    const barkCount = badDog.barkCount++
+                    GuildModel.updateOne({ _id: guildModel._id, 'badDogs.userID': badDog.userID }, { '$set': { 'badDogs.$.barkCount': barkCount } })
+                    console.log(`${pup.id} is a bad dog.\tUserID:${badDog.userID}\tBarkCount:${badDog.barkCount}`);
 
                     const replyText = `# **${barkList[Math.floor(Math.random() * barkList.length)]}**\n\n` +
-                    `*Womp Womp*, /Pet was not effective. This is your ${barkcount}/${maxBarks * (Math.floor((barkcount - 1) / maxBarks) + 1)} strike. ${barkcount % maxBarks === 0 ? 'Goodbye for now!' : ''}`
+                        `*Womp Womp*, /Pet was not effective. This is your ${barkCount % guildModel.maxBarks}/${guildModel.maxBarks} strike. ${barkCount % guildModel.maxBarks === 0 ? 'Goodbye for now!' : ''}`
                     interaction.reply(replyText)
 
-                    if (barkcount % maxBarks === 0) {
+                    if (barkCount % guildModel.maxBarks === 0) {
                         try {
-                            await (interaction.member as GuildMember).timeout(60_000)
+                            await interaction.guild.members.cache.get(interaction.user.id).timeout(60_000)
                         } catch (e) {
                             console.error('Did not have permissions, likely a higher role than the bot.');
                             console.error(e.rawError);
                         }
                     }
-                })()
+                }
             } else {
                 interaction.reply('# Wags Tail')
             }
         default:
             break
     }
+})
+
+client.on(Events.GuildAvailable, async guild => {
+    await GuildModel.findOneAndUpdate({guildID: guild.id}, {'$setOnInsert': {}}, {upsert: true})
+    
+    console.log(`Available: ${guild.name}`);
+})
+
+client.on(Events.GuildCreate, async guild => {
+    await GuildModel.findOneAndUpdate({guildID: guild.id}, {'$setOnInsert': {}}, {upsert: true})
+
+    console.log(`Joined: ${guild.name}`);
+})
+
+client.on(Events.GuildDelete, async guild => {
+    await GuildModel.deleteOne({guildID: guild.id})
 })
 
 client.login(env['TOKEN'])
--- src/model/guild.ts
+++ src/model/guild.ts
@@ -1,6 +1,28 @@
+import { Model, Schema, Types, model } from "mongoose"
 
+
+interface IGuildBadDog {
+    userID: string,
+    barkCount: number
+}
 
 interface IGuild {
     guildID: string,
-    badDogs
+    maxBarks: number,
+    badDogs: Types.Array<IGuildBadDog>
 }
+
+const GuildBadDogSchema = new Schema<IGuildBadDog, Model<IGuildBadDog>>({
+    userID: {type: String, required: true},
+    barkCount: {type: Number, required: true}
+})
+
+const GuildSchema = new Schema<IGuild, Model<IGuild>> ({
+    guildID: {type: String, required: true, unique: true},
+    maxBarks: {type: Number, required: true, default: 3},
+    badDogs: [GuildBadDogSchema]
+})
+
+const GuildModel = model<IGuild>('Guild', GuildSchema)
+
+export default GuildModel
